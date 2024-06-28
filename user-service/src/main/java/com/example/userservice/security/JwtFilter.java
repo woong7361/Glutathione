80b89ac9f621ca530ad1c71152d5d0fbe35b86bc
woong <woong7361@gmail.com>
2024-06-28 15:49:02 +0900

    exception: add jwt exception handler

--- user-service/src/main/java/com/example/userservice/security/JwtFilter.java
+++ user-service/src/main/java/com/example/userservice/security/JwtFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
