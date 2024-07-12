86e45f20c4928fc15fe3417dd8d4c216c20067f1
woong <woong7361@gmail.com>
2024-07-12 14:52:44 +0900

    feat: jwt expired exception handling

--- gateway-service/src/main/java/com/example/gatewayservice/service/JwtService.java
+++ gateway-service/src/main/java/com/example/gatewayservice/service/JwtService.java
@@ -17,27 +17,14 @@ import java.util.Base64;
 @Slf4j
 public class JwtService {
     private final Environment env;
-    public Boolean verify(String jwt, String propertyKey) {
+    public void verify(String jwt, String propertyKey) {
         SecretKey signingKey = getSecretKey(propertyKey);
 
-        String subject;
-        try {
-            JwtParser jwtParser = Jwts.parser()
-                    .setSigningKey(signingKey)
-                    .build();
+        JwtParser jwtParser = Jwts.parser()
+                .setSigningKey(signingKey)
+                .build();
 
-            subject = jwtParser.parseClaimsJws(jwt).getBody().getSubject();
-        } catch (Exception ex) {
-            log.error("authentication Error : {}", ex.getMessage());
-            return false;
-        }
-
-        if (subject == null || subject.isEmpty()) {
-            log.error("authentication Error memberId is: {}", subject);
-            return false;
-        }
-
-        return true;
+        jwtParser.parseClaimsJws(jwt).getBody().getSubject();
     }
 
     private SecretKey getSecretKey(String propertyKey) {
