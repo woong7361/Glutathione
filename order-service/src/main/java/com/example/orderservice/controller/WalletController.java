78bc903790ea6b0aeec8c4f1c51f856274849fcc
woong <woong7361@gmail.com>
2024-07-22 08:54:19 +0900

    feat: add wallet service

---
+++ order-service/src/main/java/com/example/orderservice/controller/WalletController.java
@@ -0,0 +1,52 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.wallet.ChargeRequestDto;
+import com.example.orderservice.resolvehandler.AuthenticationPrincipal;
+import com.example.orderservice.resolvehandler.Principal;
+import com.example.orderservice.service.WalletService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * 지갑(페이) 관련 컨트롤러
+ */
+@RestController
+@RequiredArgsConstructor
+public class WalletController {
+    public static final String AMOUNT_RESPONSE_KEY = "amount";
+    private final WalletService walletService;
+
+    /**
+     * 지갑 충전
+     * @param principal 회원 객체
+     * @param requestDto 충전 요청 dto
+     * @return 200 ok
+     */
+    @PostMapping("/wallet")
+    public ResponseEntity<Object> chargeWallet(
+            @AuthenticationPrincipal Principal principal, @Valid @RequestBody ChargeRequestDto requestDto) {
+        walletService.charge(requestDto.getAmount(), principal.getMemberId());
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 지갑 조회
+     * @param principal 회원 객체
+     * @return 지갑 충전양
+     */
+    @GetMapping("/wallet")
+    public ResponseEntity<Map<String, Long>> getWallet(@AuthenticationPrincipal Principal principal) {
+        Long walletAmount = walletService.getWalletAmount(principal.getMemberId());
+
+        return ResponseEntity
+                .ok(Map.of(AMOUNT_RESPONSE_KEY, walletAmount));
+    }
+}
---
+++ order-service/src/main/java/com/example/orderservice/repository/WalletRepository.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.repository;
+
+import com.example.orderservice.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByMemberId(Long memberId);
+}
