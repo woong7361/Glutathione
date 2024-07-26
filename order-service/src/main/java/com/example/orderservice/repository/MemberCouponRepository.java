0271315f8eb2ba6db10f190c72aab5450f8f190f
woong <woong7361@gmail.com>
2024-07-26 09:25:19 +0900

    fix: update memberCoupon using

--- order-service/src/main/java/com/example/orderservice/repository/MemberCouponRepository.java
+++ order-service/src/main/java/com/example/orderservice/repository/MemberCouponRepository.java
@@ -3,6 +3,7 @@ package com.example.orderservice.repository;
 import com.example.orderservice.dto.coupon.CouponResponseDto;
 import com.example.orderservice.entity.MemberCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -21,6 +22,7 @@ public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long
             "WHERE mc.memberId = :memberId AND (c.productId = :productId OR c.productId IS NULL)")
     List<MemberCoupon> findAvailableCoupons(Long memberId, Long productId);
 
+    @Modifying
     @Query("UPDATE MemberCoupon mc " +
             "SET mc.isUsed = true " +
             "WHERE mc.memberCouponId = :memberCouponId")
